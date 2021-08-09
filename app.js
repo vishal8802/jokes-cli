d9ab40357af967e98804184e11b89b94187e8d11
Vishal Singh <vishal.singh@unthinkable.co>
2021-08-09 23:31:14 +0530

    added colored logs

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-// console.log(process.argv);
 const { prompt } = require('./utils/prompt');
 const request = require('./utils/request');
 const { addJoke, getPopularJoke, clearJokes } = require('./utils/fs');
+const chalk = require('chalk');
+const log = console.log;
 
 const BASE_URL = `https://icanhazdadjoke.com`;
 let term = ``;
@@ -9,27 +10,25 @@ let term = ``;
 const init = async () => {
     if (process.argv.includes('leaderboard') && process.argv.includes('--clear')) {
         clearJokes();
-        console.log('Leaderboard cleared')
+        log(chalk.black.bgMagenta('\nLeaderboard cleared\n'))
         process.exit();
     }
 
     if (process.argv.includes('leaderboard')) {
         const popularJoke = getPopularJoke();
         if(!popularJoke){
-            console.log('No jokes in leaderboard');
+            log(chalk.black.bgMagenta('\nNo jokes in leaderboard\n'));
             process.exit();
 
         }
-        console.log('Here is a popular joke for you')
-        console.log(popularJoke);
+        log(chalk.cyan('\nHere is a popular joke for you\n'));
+        log(chalk.black.bgCyan(popularJoke + '\n\n'));
         process.exit();
     }
 
-   
-
     const input = await prompt.get(['term']);
     term = input.term;
-
+    console.clear();
     const jokesResponse = await request({
         method: 'GET',
         url: `${BASE_URL}/search` + (term ? `?term=${term}` : ''),
@@ -38,18 +37,19 @@ const init = async () => {
 
     const jokes = JSON.parse(jokesResponse).results;
     if (!jokes.length) {
-        console.log(`No jokes were found for the search term: ${term}`);
+        log(chalk.red(`\nNo jokes were found for the search term: ${term}\n`));
         process.exit();
     }
 
     const random = Math.floor(Math.random() * jokes.length);
     const randomJoke = jokes[random].joke;
-    console.log(`Here is a joke for you`);
-    console.log(randomJoke);
+    log(chalk.yellow('\nHere is a joke for you\n'));
+    log(chalk.black.bgYellow(randomJoke))
+    log('\n')
     addJoke(randomJoke);
 };
 
 init().catch((err) => {
-    console.log(err);
-    console.log('Something went wrong');
+    console.log(err)
+    log(chalk.red.bold('Something went wrong'));
 });
